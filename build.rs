df79a1342df08313151c291e422c327e0bcfa4b6
Maxwell Koo <mjkoo90@gmail.com>
2015-11-05 00:03:58 -0500

    Static bindings

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-// Based heavily on StaticStructGenerator from the gl_generator crate, original copyright below:
+// Based heavily on StaticGenerator from the gl_generator crate, original copyright below:
 //
 // Copyright 2013-2014 The gl-rs developers. For a full listing of the authors,
 // refer to the AUTHORS file at the top-level directory of this distribution.
@@ -27,15 +27,14 @@ use gl_generator::registry::{Registry, Ns};
 use std::io;
 
 #[allow(missing_copy_implementations)]
-struct EpoxyStructGenerator;
+struct EpoxyGenerator;
 
-impl gl_generator::generators::Generator for EpoxyStructGenerator {
+impl gl_generator::generators::Generator for EpoxyGenerator {
     fn write<W>(&self, registry: &Registry, ns: Ns, dest: &mut W) -> io::Result<()> where W: io::Write {
         try!(write_header(dest));
         try!(write_type_aliases(&ns, dest));
         try!(write_enums(registry, dest));
-        try!(write_struct(&ns, dest));
-        try!(write_impl(registry, &ns, dest));
+        try!(write_get_proc_address(registry, &ns, dest));
         try!(write_fns(registry, &ns, dest));
         Ok(())
     }
@@ -48,13 +47,14 @@ fn write_header<W>(dest: &mut W) -> io::Result<()> where W: io::Write {
 mod __gl_imports {{
     extern crate libc;
     pub use std::mem;
+    pub use std::ptr;
 }}"#
     )
 }
 
 /// Creates a `types` module which contains all the type aliases.
 ///
-/// See also `generators::gen_type_aliases`.
+/// See also `gl_generator::generators::gen_type_aliases`.
 fn write_type_aliases<W>(ns: &Ns, dest: &mut W) -> io::Result<()> where W: io::Write {
     try!(writeln!(dest, r#"
 pub mod types {{
@@ -78,60 +78,62 @@ fn write_enums<W>(registry: &Registry, dest: &mut W) -> io::Result<()> where W: 
     Ok(())
 }
 
-/// Creates a stub structure.
-///
-/// The name of the struct corresponds to the namespace.
-fn write_struct<W>(ns: &Ns, dest: &mut W) -> io::Result<()> where W: io::Write {
-    writeln!(dest, r#"
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(dead_code)]
-#[derive(Copy, Clone)]
-pub struct {ns};"#,
-        ns = ns.fmt_struct_name(),
-    )
-}
-
-/// Creates the `impl` of the structure created by `write_struct`.
-fn write_impl<W>(registry: &Registry, ns: &Ns, dest: &mut W) -> io::Result<()> where W: io::Write {
-    try!(writeln!(dest, r#"impl {ns} {{"#, ns = ns.fmt_struct_name()));
+/// Creates all the functions corresponding to the GL bindings.
+fn write_get_proc_address<W>(registry: &Registry, ns: &Ns, dest: &mut W) -> io::Result<()> where W: io::Write {
+    try!(writeln!(dest, r#"
+pub fn get_proc_address(symbol: &str) -> *const __gl_imports::libc::c_void {{
+    unsafe {{
+        match &symbol[..] {{"#
+    ));
 
     for c in registry.cmd_iter() {
         try!(writeln!(dest, r#"
-#[allow(non_snake_case, unused_variables, dead_code)] #[inline]
-pub unsafe fn {name}(&self, {typed_params}) -> {return_suffix} {{
-    __gl_imports::mem::transmute::<_, extern "system" fn({typed_params}) -> {return_suffix}>(epoxy_{name})({idents})
+        "{symbol}" => __gl_imports::mem::transmute::<_, *const __gl_imports::libc::c_void>({name}),"#,
+            symbol = gl_generator::generators::gen_symbol_name(ns, &c.proto.ident),
+            name = c.proto.ident,
+        ));
+    }
+
+    try!(writeln!(dest, r#"_ => __gl_imports::ptr::null(),"#));
+
+    writeln!(dest, r#"}}}}}}"#)
+}
+
+/// Creates all the functions corresponding to the GL bindings.
+fn write_fns<W>(registry: &Registry, ns: &Ns, dest: &mut W) -> io::Result<()> where W: io::Write {
+    for c in registry.cmd_iter() {
+        try!(writeln!(dest, r#"
+#[allow(non_snake_case, unused_variables, dead_code)]
+pub extern fn {name}({typed_params}) -> {return_suffix} {{
+    unsafe {{
+        __gl_imports::mem::transmute::<_, extern "system" fn({typed_params}) -> {return_suffix}>(ffi::epoxy_{symbol})({idents})
+    }}
 }}"#,
             name = c.proto.ident,
+            symbol = gl_generator::generators::gen_symbol_name(ns, &c.proto.ident),
             typed_params = gl_generator::generators::gen_parameters(c, true, true).join(", "),
             return_suffix = gl_generator::generators::gen_return_type(c),
             idents = gl_generator::generators::gen_parameters(c, true, false).join(", "),
         ));
     }
 
-    writeln!(dest, r#"}}"#)
-}
-
-/// io::Writes all functions corresponding to the GL bindings.
-///
-/// These are foreign functions, they don't have any content.
-fn write_fns<W>(registry: &Registry, ns: &Ns, dest: &mut W) -> io::Result<()> where W: io::Write {
     try!(writeln!(dest, r#"
 #[allow(non_snake_case)]
 #[allow(unused_variables)]
 #[allow(dead_code)]
-extern "system" {{"#
+mod ffi {{
+    use __gl_imports;
+    extern "system" {{"#
     ));
 
     for c in registry.cmd_iter() {
         try!(writeln!(dest, r#"
-#[link_name="epoxy_{symbol}"] static epoxy_{name}: *const *const __gl_imports::libc::c_void;"#,
+#[link_name="epoxy_{symbol}"] pub static epoxy_{symbol}: *const *const __gl_imports::libc::c_void;"#,
             symbol = gl_generator::generators::gen_symbol_name(ns, &c.proto.ident),
-            name = c.proto.ident,
         ));
     }
 
-    writeln!(dest, r#"}}"#)
+    writeln!(dest, r#"}}}}"#)
 }
 
 pub fn main() {
@@ -140,70 +142,10 @@ pub fn main() {
 
     let mut file = BufWriter::new(File::create(&dest.join("bindings.rs")).unwrap());
 
-    // From glium build/main.rs
-    let extensions = vec![
-        "GL_AMD_depth_clamp_separate".to_string(),
-        "GL_APPLE_vertex_array_object".to_string(),
-        "GL_ARB_bindless_texture".to_string(),
-        "GL_ARB_buffer_storage".to_string(),
-        "GL_ARB_compute_shader".to_string(),
-        "GL_ARB_copy_buffer".to_string(),
-        "GL_ARB_debug_output".to_string(),
-        "GL_ARB_depth_texture".to_string(),
-        "GL_ARB_direct_state_access".to_string(),
-        "GL_ARB_draw_buffers".to_string(),
-        "GL_ARB_ES2_compatibility".to_string(),
-        "GL_ARB_ES3_compatibility".to_string(),
-        "GL_ARB_ES3_1_compatibility".to_string(),
-        "GL_ARB_ES3_2_compatibility".to_string(),
-        "GL_ARB_framebuffer_sRGB".to_string(),
-        "GL_ARB_geometry_shader4".to_string(),
-        "GL_ARB_gpu_shader_fp64".to_string(),
-        "GL_ARB_gpu_shader_int64".to_string(),
-        "GL_ARB_invalidate_subdata".to_string(),
-        "GL_ARB_multi_draw_indirect".to_string(),
-        "GL_ARB_occlusion_query".to_string(),
-        "GL_ARB_pixel_buffer_object".to_string(),
-        "GL_ARB_robustness".to_string(),
-        "GL_ARB_shader_image_load_store".to_string(),
-        "GL_ARB_shader_objects".to_string(),
-        "GL_ARB_texture_buffer_object".to_string(),
-        "GL_ARB_texture_float".to_string(),
-        "GL_ARB_texture_multisample".to_string(),
-        "GL_ARB_texture_rg".to_string(),
-        "GL_ARB_texture_rgb10_a2ui".to_string(),
-        "GL_ARB_transform_feedback3".to_string(),
-        "GL_ARB_vertex_buffer_object".to_string(),
-        "GL_ARB_vertex_shader".to_string(),
-        "GL_ATI_draw_buffers".to_string(),
-        "GL_ATI_meminfo".to_string(),
-        "GL_EXT_debug_marker".to_string(),
-        "GL_EXT_direct_state_access".to_string(),
-        "GL_EXT_framebuffer_blit".to_string(),
-        "GL_EXT_framebuffer_multisample".to_string(),
-        "GL_EXT_framebuffer_object".to_string(),
-        "GL_EXT_framebuffer_sRGB".to_string(),
-        "GL_EXT_gpu_shader4".to_string(),
-        "GL_EXT_packed_depth_stencil".to_string(),
-        "GL_EXT_provoking_vertex".to_string(),
-        "GL_EXT_texture_array".to_string(),
-        "GL_EXT_texture_buffer_object".to_string(),
-        "GL_EXT_texture_compression_s3tc".to_string(),
-        "GL_EXT_texture_filter_anisotropic".to_string(),
-        "GL_EXT_texture_integer".to_string(),
-        "GL_EXT_texture_sRGB".to_string(),
-        "GL_EXT_transform_feedback".to_string(),
-        "GL_GREMEDY_string_marker".to_string(),
-        "GL_KHR_robustness".to_string(),
-        "GL_NVX_gpu_memory_info".to_string(),
-        "GL_NV_conditional_render".to_string(),
-        "GL_NV_vertex_attrib_integer_64bit".to_string(),
-    ];
-
-    gl_generator::generate_bindings(EpoxyStructGenerator,
+    gl_generator::generate_bindings(EpoxyGenerator,
                                     gl_generator::registry::Ns::Gl,
                                     gl_generator::Fallbacks::None,
-                                    khronos_api::GL_XML, extensions,
-                                    "4.5", "compatibility",
+                                    khronos_api::GL_XML, vec![],
+                                    "4.5", "core",
                                     &mut file).unwrap();
 }
